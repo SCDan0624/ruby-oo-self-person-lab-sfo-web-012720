8ed253a127f21eef3a4d3922e4e0a231ed57ffc8
Daniel Mayle <dmayle012@gmail.com>
2020-01-29 18:51:11 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,50 @@
+# your code goes here
+class Person
+ 
+    attr_reader :name
+    attr_accessor :bank_account
+ 
+    def initialize(person_name)
+        @name = person_name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+ 
+    def happiness=(happiness)
+ 
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        else
+           @happiness = happiness
+        end
+       
+    end
+ 
+    def happiness
+        @happiness
+    end
+ 
+    def hygiene=(hygiene)
+ 
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        else
+           @hygiene = hygiene
+        end
+       
+    end
+ 
+    def hygiene
+        @hygiene
+    end
+ 
+    def happy?
+        @happiness > 7
+    end
+ 
+end
