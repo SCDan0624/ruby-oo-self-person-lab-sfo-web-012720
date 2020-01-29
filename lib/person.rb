d76d99853748bfcd62bd81ed8cf96df8993e163b
Daniel Mayle <dmayle012@gmail.com>
2020-01-29 19:45:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,3 @@
-# your code goes here
 class Person
  
     attr_reader :name
@@ -46,20 +45,46 @@ class Person
     def happy?
         @happiness > 7
     end
-    
+   
     def clean?
         @hygiene > 7
     end
-    
+   
     def get_paid(amount)
       @bank_account += amount
       "all about the benjamins"
     end
-    
+   
     def take_bath
       self.hygiene += 4
        "♪ Rub-a-dub just relaxing in the tub ♫"
     end
  
-  
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+ 
+    def call_friend(person)
+ 
+        self.happiness += 3
+        person.happiness += 3
+ 
+        "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+ 
+    def start_conversation(person, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
 end
